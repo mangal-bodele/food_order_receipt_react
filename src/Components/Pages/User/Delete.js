1c226ad3773caffafc49ffba05acb59ca21018c7
mangal <mangalbodele123@gmail.com>
2024-04-16 08:08:23 +0530

    initial commit

--- src/Components/Pages/User/Delete.js
+++ src/Components/Pages/User/Delete.js
@@ -7,8 +7,8 @@ function Delete() {
   const navi= useNavigate()
   const {userId} = useParams()
 
-  function fetchdata(){
-    const result = axios.get(`http://localhost:5000/users/${userId}/`)
+  async function fetchdata(){
+    const result = await axios.get(`http://localhost:5000/users/${userId}/`)
     setUser(result.data)
   }
   function deleteUser(){
@@ -21,8 +21,8 @@ function Delete() {
   return (
     <div className='container col-6 border border-3 p-3 mt-5'>
         <center><h1>DELETE CONFIRMATION</h1>
-        <h3>do you really want to delete <span></span>record?</h3>
-        <button onClick={deleteUser}>YES</button>&nbsp;
+        <h3>do you really want to delete <span style={{'color':'orange'}}> {user.dish} </span>from order?</h3>
+        <button onClick={()=>{deleteUser()}}>YES</button>&nbsp;
         <NavLink to='/Show'>
           <button>NO</button>
         </NavLink>
